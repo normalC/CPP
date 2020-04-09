4e4377ea52ba3f4681dad9d43f55af98db9120c9
nomalC <normala@qq.com>
2020-04-09 18:16:57 +0800

    1

---
+++ DynamicLinkLibraryDemo/math/main.c
@@ -0,0 +1,14 @@
+//math.c
+#include<stdio.h>
+int add(int a,int b){
+    return a+b;
+}
+int sub(int a,int b){
+    return a-b;
+}
+int mul(int a,int b){
+    return a*b;
+}
+int div(int a,int b){
+    return a/b;
+}
