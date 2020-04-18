13f44e0a34ff6066d903599af5f7c1be417d7fdb
normalC <39472288+normalC@users.noreply.github.com>
2020-04-19 00:16:47 +0800

    11

--- Reflect_callback/CReflect2/main.cpp
+++ Reflect_callback/CReflect2/main.cpp
@@ -1,32 +1,39 @@
 #include <iostream>
 #include <map>
 
+
 using namespace std;
 
-
-
 class Base{
+
 public:
-Base(){}
+B (){}
 };
 
 class C:public Base {
 public:
 C(){}
-};
-
-clas A:public Base {
-public:
-A(){};
-
-};
-
-class B:public Base {
-public:
-B(){};
+void show(void){cout<<"show"<<endl;}
 };
 
 
+
+typedef Base* (*getBase) ();
+
+map<string, getBase> generatorMap;
+
+#define CREATE_GENERATOR(class_name) \
+static Base* gen_##class_name(){ \
+return new class_name(); \
+}
+
+#define REGISTRY_CLASS_GENERATOR(class_name) \
+CREATE_GENERATOR(class_name); \
+generatorMap[#class_name] = &gen_##class_name;
+
+
+
+map<string, Base*> instanceMap;
 
 #define REGISTRY_CLASS(class_name) \
 class registry_##class_name { \
@@ -37,10 +44,34 @@ instanceMap[#class_name] = new class_name(); \
 }; registry_##class_name registry_##class_name##inst;
 
 
+
 int main()
 
-{  map<string, Base*> instanceMap;
-    REGISTRY_CLASS(A)
-    cout << "Hello world!" << endl;
+{
+    REGISTRY_CLASS(C)
+
+/*    展开之后
+    class registry_C {
+        public:
+            registry_C() {
+                instanceMap["C"] = new C();}
+            show(){
+                cout<<"show"<<endl;}};
+    registry_C registry_Cinst;*/
+
+
+    Base* inst = (generatorMap["C"])();
+
+
+
+
     return 0;
 }
+
+
+
+
+
+
+
+
