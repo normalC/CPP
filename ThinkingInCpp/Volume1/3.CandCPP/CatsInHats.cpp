af15a088df95da7f23f069e415d6b388232854be
nomalC <normala@qq.com>
2020-05-12 14:16:14 +0800

    1

--- ThinkingInCpp/Volume1/3.CandCPP/CatsInHats.cpp
+++ ThinkingInCpp/Volume1/3.CandCPP/CatsInHats.cpp
@@ -1,17 +1,30 @@
-//: C03:PointerToFunction.cpp
-// Defining and using a pointer to a function
+//: C03:FunctionTable.cpp
+// Using an array of pointers to functions
 #include <iostream>
 using namespace std;
 
-void func(void) {
-  cout << "func() called..." << endl;
-}
+// A macro to define dummy functions:
+#define DF(N) void N() { \
+   cout << "function " #N " called..." << endl; }
+
+DF(a); DF(b); DF(c); DF(d); DF(e); DF(f); DF(g);
+
+void (*func_table[])() = { a, b, c, d, e, f, g };
 
 int main() {
-  void (*fp)(void);  // Define a function pointer
-  fp = func;  // Initialize it
-  (*fp)();    // Dereferencing calls the function
-  void (*fp2)(void) = func;  // Define and initialize
-  (*fp2)();
+  while(1) {
+    cout << "press a key from 'a' to 'g' "
+      "or q to quit" << endl;
+    char c, cr;
+    cin.get(c); cin.get(cr); // second one for CR
+    if ( c == 'q' ) 
+      break; // ... out of while(1)
+    if ( c < 'a' || c > 'g' ) 
+      continue;
+    cout << "c - 'a':"<<c - 'a'<< endl;
+	cout <<func_table[c - 'a']<< endl;
+    (*func_table[c - 'a'])();
+  }
 } ///:~
-// After the pointer 
+
+
